618da91c05989b6220e75c8e00a273e5c20f6f10
adkow <a.kowalewski.student@gmail.com>
2020-04-25 12:12:44 +0200

    dodanie runnerapp i zmiany w MainFrame

---
+++ src/main/java/RunnerApp.java
@@ -0,0 +1,8 @@
+import gui.MainFrame;
+
+public class RunnerApp {
+
+    public static void main(String[] args) {
+        new MainFrame().startGui();
+    }
+}
--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
+
     public void startGui() {
         JFrame mainFrame = new JFrame("Crypto app");
         mainFrame.setMinimumSize(new Dimension(400, 200));
-        mainFrame.setLocation(300, 300);
+        mainFrame.setLocationRelativeTo(null);
         mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         mainFrame.add(addMainPanel());
         mainFrame.setVisible(true);
@@ -16,15 +17,19 @@ public class MainFrame extends JFrame {
     private Component addMainPanel() {
         JPanel panel = new JPanel();
         GridBagLayout layout = new GridBagLayout();
+        panel.setLayout(layout);
         GridBagConstraints constr = new GridBagConstraints();
+
         //TODO get this value from properties
         JLabel titleLabel = new JLabel("Crypto");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 28));
         titleLabel.setForeground(Color.RED);
+
         constr.fill = GridBagConstraints.HORIZONTAL;
         constr.gridx = 0;
         constr.gridy = 0;
-        layout.addLayoutComponent(titleLabel, constr);
+        panel.add(titleLabel, constr);
+
         return panel;
     }
 }
